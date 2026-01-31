983a7cb3637275cf26049cb32b5ffd06038656fb
hagarMarzouk99 <eng.hagarabdo@gmail.com>
2026-01-31 22:42:38 +0200

    basic_oop

--- task1/bin/main.dart
+++ task1/bin/main.dart
@@ -1,10 +1,24 @@
 class Person {
   int age = 22 ;
-  String name = "hagar" ;
+  String name = "hagar";
+  String email = 'hagar@gmail.com';
+
+  Person({required this.name , required this.age , required this.email});   // constructor 
+
+  void printDetails (){
+    print('name : $name');
+    print('email : $email');
+    print('age : $age');
+
+  }
+
 
 }
 void main (){
-  Person person1 = Person();
-  print(person1.age);
-  print(person1.name);
+  Person person1 = Person( name: 'Hagar Marzouk' , age: 21 ,  email: 'hagar@gmail.com' );
+  Person person2 = Person( name: 'Noura' , age: 21 ,  email: 'noura@gmail.com' );
+  person1.printDetails();
+  print('==================');
+  person2.printDetails();
+ 
 }
